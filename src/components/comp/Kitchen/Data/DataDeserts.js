80ba9e62dc4aea65d13b34e4734e426d8a36f009
Олексій Демидов <orange.family.cn@gmail.com>
2022-11-21 12:48:47 +0200

    fix

--- src/components/comp/Kitchen/Data/DataDeserts.js
+++ src/components/comp/Kitchen/Data/DataDeserts.js
@@ -3,14 +3,14 @@ const DesertsMass=[
         id:1,
         title:"торт шоколадно-помаранчевий 100г",
         price:"89 грн",
-        src: require('../img/tort.png'),
+        src: require('../img/tort.webp'),
         text:"",
     },
     {
         id:2,
         title:"морозиво 100г",
         price:"59 грн",
-        src: require('../img/icee.png'),
+        src: require('../img/icee.webp'),
         text:"пломбір, полуничне, шоколадне",
     },
    
