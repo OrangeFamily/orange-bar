f2b4339a245397dc94b29f68d221791baf44ebdd
OrangeFamily <111183461+OrangeFamily@users.noreply.github.com>
2024-05-01 20:22:09 +0300

    Update DataColdTea.js

--- src/components/comp/Cocotail/Data/DataColdTea.js
+++ src/components/comp/Cocotail/Data/DataColdTea.js
@@ -1,11 +1,11 @@
 const ColdTeaMass=[
-    {
-        id:1,
-        title:"Імбирно-м’ятний 450мл",
-        price:"65 грн",
-        src: require('../img/iceteanew.png'),
-        text:"чай зелений, м’ята, імбир",
-    },
+   // {
+     //  id:1,
+       // title:"Імбирно-м’ятний 450мл",
+       // price:"65 грн",
+       // src: require('../img/iceteanew.png'),
+      //  text:"чай зелений, м’ята, імбир",
+   // },
     {
         id:2,
         title:"Цитрусовий 450мл",
@@ -13,13 +13,13 @@ const ColdTeaMass=[
         src: require('../img/iceteanew.png'),
         text:"чай зелений, лимон, апельсин, лайм",
     },
-    {
-        id:3,
-        title:"Фруктовий 450мл",
-        price:"65 грн",
-        src: require('../img/iceteanew.png'),
-        text:"чай зелений, сироп в асортименті",
-    },
+   // {
+      //  id:3,
+     //   title:"Фруктовий 450мл",
+      //  price:"65 грн",
+      //  src: require('../img/iceteanew.png'),
+      //  text:"чай зелений, сироп в асортименті",
+   // },
 
 
     
