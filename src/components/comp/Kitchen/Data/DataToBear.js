7d889f3c880b9b21f7247a27bfaa0a14d8883669
OrangeFamily <111183461+OrangeFamily@users.noreply.github.com>
2024-05-15 17:02:58 +0300

    Update DataToBear.js

--- src/components/comp/Kitchen/Data/DataToBear.js
+++ src/components/comp/Kitchen/Data/DataToBear.js
@@ -20,13 +20,13 @@ const ToBearMass=[
         src:require('../img/munhenkovb.png'),
         text:"Ковбаски, чебрець, соус гірчичний",
     },
-    {
-        id:4,
-        title:"Fish & Chips 300г/100г",
-        price:"194 грн",
-        src: require('../img/fishandchips.png'),
-        text:"Філе білої риби, темпура, картопля фрі, соус тар-тар, кетчуп, лимон",
-    },
+    //{
+        //id:4,
+        //title:"Fish & Chips 300г/100г",
+        //price:"194 грн",
+        //src: require('../img/fishandchips.png'),
+        //text:"Філе білої риби, темпура, картопля фрі, соус тар-тар, кетчуп, лимон",
+    //},
     {
         id:5,
         title:"Цибулеві кільця 150г/50г",
