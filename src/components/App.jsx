fa2d9376c053992c5a5e5f223d21d97c5e4d6abe
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-08-07 20:16:35 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -297,7 +297,7 @@ export const App = () => {
                     <AccordionItem>
                       <h2>
                         <AccordionButton className={s.titleItem}>
-                          Петельні
+                          Пательні
                         </AccordionButton>
                       </h2>
                       <AccordionPanel>
