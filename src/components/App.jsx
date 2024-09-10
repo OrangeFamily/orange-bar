8e1dbbe4043c2d2cd06d7d11b13f73ff088099b6
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-09-10 17:28:36 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,11 +18,11 @@ import { List } from './comp/List/List';
 // import fourth from './img/4.jpg'
 // import fifth from './img/5.JPG'
 
-import pn from './img/pn.jpg';
-import vt from './img/vt.jpg';
+import pn from './img/IMG_2347.JPG';
+import vt from './img/IMG_2348.JPG';
 import sr from './img/sr.jpg';
 import cht from './img/cht1.jpg';
-import ned from './img/ned.jpg';
+import ned from './img/IMG_2349.JPG';
 // import bullAct from './img/bullAct.jpg';
 // import bullActg from './img/bullActg.jpg';
 
