0e8c397a0d5f78ef574addb1a609a82fd6da09a9
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-09-13 11:30:42 +0300

    fix

--- src/components/comp/Kitchen/Data/DataPetelna.js
+++ src/components/comp/Kitchen/Data/DataPetelna.js
@@ -2,7 +2,7 @@ const PetelnaMass=[
     {
         id:14,
         title:"пательня з морепродуктами 300г",
-        price:"249 грн",
+        price:"269 грн",
         src:require('../img/skovormore.png'),
         text:"кальмар, мідії, креветка, соєвий соус, сметана, цибуля, часник",
     },
