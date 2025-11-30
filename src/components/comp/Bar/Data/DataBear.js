be586024fca06a3dfd0df61991888de28092eea1
Олексій Демидов <agdemidof@gmail.com>
2025-11-30 15:39:34 +0200

    Update DataBear.js

--- src/components/comp/Bar/Data/DataBear.js
+++ src/components/comp/Bar/Data/DataBear.js
@@ -33,14 +33,14 @@ const BearMass = [
 //     src: require('../img/somers.png'),
 //     text:"",
 // },
-  {
-      id:4,
-      title:"Повна діжка (Україна) кегове",
-      price:"0,5л - 80 грн",
-      price2:"0,33л - 65 грн",
-      src: require('../img/dizhka.png'),
-      text:"",
-  },
+  // {
+  //     id:4,
+  //     title:"Повна діжка (Україна) кегове",
+  //     price:"0,5л - 80 грн",
+  //     price2:"0,33л - 65 грн",
+  //     src: require('../img/dizhka.png'),
+  //     text:"",
+  // },
   // {
   //     id:5,
   //     title:"Хмелевус вайс (Україна) кегове",
